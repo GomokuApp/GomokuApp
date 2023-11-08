40b329b4f4e64870608c296faa8384accab0c245
RuochenFu21 <mail.by.ray@gmail.com>
2023-11-08 18:50:36 +0800

    fix: make auto-reload work

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from flask import Flask
 from gomoku import gomoku
+from os import chdir, popen
 
 app = Flask(__name__)
 app.register_blueprint(gomoku, url_prefix='/gomoku')
+
+
+@app.route("reload-website")
+def reload():
+    chdir("/home/RuochenFu/site")
+    popen("git stash")
+    popen("git stash drop")
+    return popen("git pull https://github.com/GomokuApp/GomokuApp.git")
